7d4ff97512d79fbfeb0618c540b18fc92a4521bc
paudpr <paula.p.ramirez@gmail.com>
2023-02-16 20:01:50 +0100

    fail

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-// function check_password() {
-// 	let pass1 = document.getElementById("pass")
-// 	let pass2 = document.getElementById("confirm_pass")
-// 	if (pass1 != pass2)
-// 		document.getElementById("error").classList.add("show");
-// 	else
-// 	{
-// 		document.getElementById("error").classList.remove("show");
-// 		document.getElementById("ok").classList.remove("hide");
-// 	}
-// }
 
-// // document.getElementById("password").addEventListener("focusout", validate_password);
+const na = document.getElementById("nombre");
+na.addEventListener("input", check_text);
 
+function check_text(event){
+	const input = event.target;
+	const parent_elem = event.target.parentElement;
+	const msg_error = parent_elem.parentElement.getElementsByTagName("p")[0];
 
-
-
-// // var regex_val = /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/
-
+	if (!isNaN(input.value) || input.value.trim() === "")
+	{
+		parent_elem.classList.remove("valid");
+		parent_elem.classList.add("error");
+		msg_error.textContent = msg_error.dataset.mensaje;
+	}
+	else
+	{
+		parent_elem.classList.add("valid");
+		parent_elem.classList.remove("error");
+		msg_error.textContent = "";
+	}
+	if (event.target.value == "")
+	{
+		parent_elem.classList.remove("error");
+		parent_elem.classList.remove("valid");
+		msg_error.textContent = "";
+	}
+}
 
 const em = document.getElementById("email");
-em.addEventListener("input", change_shadow);
+em.addEventListener("input", check_email);
 
-function change_shadow(event){
-	// console.log(event);
+function check_email(event){
 	const is_valid  = event.target.checkValidity();
 	const parent_elem = event.target.parentElement;
-	const msg_error = parent_elem.parentElement.getElementsByTagName("p")[0]
-	// console.log(msg_error)
+	const msg_error = parent_elem.parentElement.getElementsByTagName("p")[0];
+
 	if (is_valid)
 	{
 		parent_elem.classList.remove("error");
@@ -36,7 +44,7 @@ function change_shadow(event){
 	else{
 		parent_elem.classList.add("error");
 		parent_elem.classList.remove("valid");
-		msg_error.textContent = msg_error.dataset["mensaje"];
+		msg_error.textContent = msg_error.dataset.mensaje;
 	}
 	if (event.target.value == "")
 	{
@@ -44,134 +52,127 @@ function change_shadow(event){
 		parent_elem.classList.remove("valid");
 		msg_error.textContent = "";
 	}
-
 }
 
+const pass1 = document.getElementById("password");
+pass1.addEventListener("input", check_password);
 
-
-
-// function check_password() {
-// 	let pass1 = document.getElementById("pass")
-// 	let pass2 = document.getElementById("confirm_pass")
-// 	if (pass1 != pass2)
-// 		document.getElementById("error").classList.add("show");
-// 	else
-// 	{
-// 		document.getElementById("error").classList.remove("show");
-// 		document.getElementById("ok").classList.remove("hide");
-// 	}
-// }
-
-// // document.getElementById("password").addEventListener("focusout", validate_password);
-
-
-
-
-
-
-na.addEventListener("input", check_text);
-em.addEventListener("input", change_shadow);
-
-
-
-function check_text(event){
-	const input = document.getElementById(event);
-	const msg_error = document.getElementsByTagName("p")[0];
+function check_password(event) {
+	const pass1 = event.target;
 	const parent_elem = event.target.parentElement;
+	const msg_error = parent_elem.parentElement.getElementsByTagName("p")[0];
 	
-	if(input.value.trim() === "")
+	if (pass1.value.length > 8)
 	{
-		parent_elem.classList.remove("error");
-		parent_elem.classList.add("valid");
-		msg_error.textContent = "";
+		parent_elem.classList.remove("valid");
+		parent_elem.classList.add("error");
+		msg_error.textContent = msg_error.dataset.mensaje;
 	}
 	else
 	{
-		parent_elem.classList.add("error");
+		parent_elem.classList.add("valid");
+		parent_elem.classList.remove("error");
+		msg_error.textContent = "";
+	}
+	if (event.target.value == "")
+	{
+		parent_elem.classList.remove("error");
 		parent_elem.classList.remove("valid");
-		msg_error.textContent = msg_error.dataset["mensaje"];
+		msg_error.textContent = "";
 	}
 }
 
+const pass2 = document.getElementById("confirm-password");
+pass2.addEventListener("input", check_confirm_password);
 
-// function check_password(event){
-// 	const pass1 = document.getElementById("password");
-// 	const pass2 = docuemtn.getElementById("confirm-password");
-// 	const parent_elem = event.target.parentElement;
-// 	const msg_error = parent_elem.parentElement.getElementsByTagName("p")[0];
+function check_confirm_password(event){
+	const pass2 = event.target;
+	const parent_elem = event.target.parentElement;
+	const msg_error = parent_elem.parentElement.getElementsByTagName("p")[0];
+	const pass_orig = document.getElementById("password").value;
 
-// 	if (pass1.value.length > 8)
-// 	{
-// 		parent_elem.classList.remove("valid");
-// 		parent_elem.classList.add("error");
-// 		msg_error.textContent = msg_error.dataset["mensaje"];
-// 	}
-// 	else
-// 	{
-// 		parent_elem.classList.remove("error");
-// 		parent_elem.classList.add("valid");
-// 		msg_error.textContent= "";
-// 	}
-// 	if (event.target.value == "")
-// 	{
-// 		parent_elem.classList.remove("error");
-// 		parent_elem.classList.remove("valid");
-// 		msg_error.textContent = "";
-// 	}
-// }
+	if (pass2.value !== pass_orig)
+	{
+		parent_elem.classList.remove("valid");
+		parent_elem.classList.add("error");
+		msg_error.textContent = msg_error.dataset.mensaje;
+	}
+	else
+	{
+		parent_elem.classList.add("valid");
+		parent_elem.classList.remove("error");
+		msg_error.textContent = "";
+	}
+	if (event.target.value == "")
+	{
+		parent_elem.classList.remove("error");
+		parent_elem.classList.remove("valid");
+		msg_error.textContent = "";
+	}
+}
 
-// function check_confirm_pass(event){
-// 	const pass1 = document.getElementById("password");
-// 	const pass2 = docuemtn.getElementById("confirm-password");
-// 	const parent_elem = event.target.parentElement;
-// 	const msg_error = parent_elem.parentElement.getElementsByTagName("p")[0];
+// const form = document.querySelector('form');
+// const submit_button = document.getElementById('send_button');
+// const inputs = form.querySelectorAll('input');
 
-// 	if (pass1.value.length > 8)
-// 	{
-// 		parent_elem.classList.remove("valid");
-// 		parent_elem.classList.add("error");
-// 		msg_error.textContent = msg_error.dataset["mensaje"];
-// 	}
-// 	else
-// 	{
-// 		parent_elem.classList.remove("error");
-// 		parent_elem.classList.add("valid");
-// 		msg_error.textContent= "";
-// 	}
-// 	if (event.target.value == "")
-// 	{
-// 		parent_elem.classList.remove("error");
-// 		parent_elem.classList.remove("valid");
-// 		msg_error.textContent = "";
-// 	}
-// }
+// inputs.forEach((input) => {
+//   input.addEventListener('input', check_valid_input);
+// });
 
 
 
 
-// function check_password() {
-// 	const passwordInput = document.getElementById("password");
-// 	const errorIcon = passwordInput.nextElementSibling;
-// 	const successIcon = errorIcon.nextElementSibling;
-// 	const errorText = passwordInput.parentElement.querySelector("[data-mensaje]");
-  
-// 	if (passwordInput.value.length > 8) {
-// 	  // Si el valor tiene más de 8 caracteres, muestra un mensaje de error
-// 	  errorIcon.style.display = "block";
-// 	  successIcon.style.display = "none";
-// 	  errorText.style.display = "block";
-// 	  return false;
-// 	} else {
-// 	  // Si el valor tiene 8 o menos caracteres, oculta el mensaje de error y muestra el icono de éxito
-// 	  errorIcon.style.display = "none";
-// 	  successIcon.style.display = "block";
-// 	  errorText.style.display = "none";
-// 	  return true;
-// 	}
-//   }
-  
-//   // Agrega el event listener para el evento "input" en la carga de la página
-//   document.addEventListener("DOMContentLoaded", function() {
-// 	const passwordInput = document.getElementById("password");
-// 	passwordInput.addEventListener("input", check_password);
-//   });
+function check_valid_input(event)
+{
+	var id_bt = document.getElementById("button");
+
+		// alert("lol");
+	do_alert_valid(event);
+	// funcion que te valide id a id de los campos si son validos o no -> return alerta aqui;
+	// sino, alerta en esa funcion
+   
+}
+
+function do_alert_valid(event)
+{
+	var  input_list = document.getElementById("nombre");
+	let  valid = true;
+	const msg_error = input_list.target.parentElement.parentElement.getElementsByTagName("p")[0];
+
+	// alert(input_list.value);
+	if (input_list.value == "")
+	{
+		alert("sdfgdsg");
+
+	}
+		
+	
+}
+
+/*
+function check_valid_input() 
+{
+  let valid = true;
+
+  inputs.forEach((input) => {
+    if (input.value.trim() === '')
+	{
+      valid = false;
+      input.parentElement.classList.remove('valid');
+      input.parentElement.classList.add('error');
+      input.parentElement.parentElement.querySelector('p').textContent = 'Rellene este campo';
+    } 
+	else if (input.parentElement.classList.contains('error'))
+	{
+      valid = false;
+    } 
+	else
+	{
+      input.parentElement.classList.remove('error');
+      input.parentElement.classList.add('valid');
+    }
+  });
+
+  submit_button.disabled = !valid;
+}
+*/
